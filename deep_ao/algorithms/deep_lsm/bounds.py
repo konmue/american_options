fixing upper bound next paths calculation

deep_ao/algorithms/deep_lsm/bounds.py
@@ -1,13 +1,10 @@
-import sys
+import gc
 from typing import Callable
 
 import numpy as np
 import torch
-from grpc import Call
 from scipy.stats import norm
 from tqdm import tqdm
-
-from deep_ao.data.geometric_bm import geometric_bm_generator
 
 
 def calculate_lower_bound(
@@ -60,7 +57,9 @@ def calculate_payoffs_at_stop(
 
         if n != 0:
             continuation_values = (
-                models[f"model_{n}"](torch.tensor(np.c_[x_n, payoff(n, x_n)]).float())
+                models[f"model_{n}"](
+                    torch.from_numpy(np.c_[x_n, payoff(n, x_n)]).float()
+                )
                 .squeeze()
                 .detach()
                 .numpy()
@@ -106,14 +105,18 @@ def calculate_upper_bound(
                 n_steps=n_steps - n,
                 n_simulations=n_nested_paths,
             )
+            paths_from_here = paths_from_here[:, 1:]
             continuation_value = calculate_payoffs_at_stop(
                 paths_from_here, payoff, models, n_steps, time=n
             ).mean()
             # check dim here; if cont value includes payoff now; could this be wrong?
             all_continuation_values[i, n] = continuation_value
 
+            del paths_from_here
+            gc.collect()
+
         model_continuation_values = (
-            models[f"model_{n}"](torch.tensor(np.c_[x_n, payoff(n, x_n)]).float())
+            models[f"model_{n}"](torch.from_numpy(np.c_[x_n, payoff(n, x_n)]).float())
             .squeeze()
             .detach()
             .numpy()

deep_ao/algorithms/deep_lsm/deep_lsm.py
@@ -1,3 +1,4 @@
+import gc
 from typing import Callable
 
 import numpy as np
@@ -5,10 +6,9 @@ import pytorch_lightning as pl
 import torch
 from torch.utils.data import DataLoader
 
+from deep_ao.algorithms.deep_lsm.config import MAX_EPOCHS
 from deep_ao.data.utils import prepare_training_data
 from deep_ao.models.fnn import FNN, FNNParams
-
-MAX_EPOCHS = 1  # not using the same paths twice
 
 
 def deep_lsm(
@@ -36,7 +36,7 @@ def deep_lsm(
         # paths at point n
         x_n = paths[:, n, :]
 
-        # option payoff if exercise now (not necassary, but usefule additional feature)
+        # option payoff if exercise now (not necessary, but usefule additional feature)
         payoff_now = payoff(n, x_n)
 
         dataset = prepare_training_data(
@@ -52,7 +52,7 @@ def deep_lsm(
             model = FNN(fnn_params_others)
             model.load_state_dict(state_dict)
 
-        trainer = pl.Trainer(max_epochs=MAX_EPOCHS)
+        trainer = pl.Trainer(max_epochs=MAX_EPOCHS, enable_model_summary=False)
         trainer.fit(model, dataloader)
 
         # saving the model
@@ -67,6 +67,10 @@ def deep_lsm(
             .squeeze()
         )
         stopping_times[idx] = n
+
+        del dataset
+        del dataloader
+        gc.collect()
 
     # value at time 0 (price) given by mean payoff at optimal stopping times
     models["model_0"] = payoff(stopping_times, stopped_paths).mean()

deep_ao/data/geometric_bm.py
@@ -11,10 +11,9 @@ def geometric_bm_generator(
     interest_rate: float,
     dividend_yield: float,
     sigma: float,
-    maturity: float,
+    delta_t: float,
 ) -> np.ndarray:
 
-    delta_t = maturity / n_steps
     bm_increments = np.random.normal(
         scale=np.sqrt(delta_t), size=(n_simulations, n_steps, dim)
     )