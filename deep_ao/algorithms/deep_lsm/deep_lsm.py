f5ffca37f2cae7de6a4295c06973fdaec18364cc
konrad <mail@konrad-mueller.com>
2022-05-07 12:17:45 +0200

    cleaning dlsm + adding del and garbage collection

--- deep_ao/algorithms/deep_lsm/deep_lsm.py
+++ deep_ao/algorithms/deep_lsm/deep_lsm.py
@@ -4,10 +4,9 @@ from typing import Callable
 import numpy as np
 import pytorch_lightning as pl
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, TensorDataset
 
 from deep_ao.config import MAX_EPOCHS
-from deep_ao.data.utils import prepare_training_data
 from deep_ao.models.fnn import FNNLightning, FNNLightningParams
 
 
@@ -20,28 +19,20 @@ def train(
 ):
     """The LSM algorithm with neural networks as in Section 2 of the paper."""
 
-    n_paths = paths.shape[0]
     n_steps = paths.shape[1] - 1
     models = {}
 
-    # stopping times s initialized to last period
-    stopping_times = (np.ones(n_paths) * n_steps).astype(int)
+    payoff_at_stop = payoff_fn(n_steps, paths[:, -1])
 
     for n in np.arange(start=n_steps - 1, stop=0, step=-1):
         print(f"Training network {n}")
 
-        # paths at (so far) optimal stopping time
-        stopped_paths = paths[np.arange(n_paths), stopping_times, :]
-
-        # paths at point n
         x_n = paths[:, n]
-
-        # option payoff if exercise now (not necessary, but usefule additional feature)
         payoff_now = payoff_fn(n, x_n)
 
-        dataset = prepare_training_data(
-            x_n, payoff_now, stopping_times, stopped_paths, payoff_fn
-        )
+        model_input = torch.tensor(np.c_[x_n, payoff_now]).float()
+        targets = torch.unsqueeze(torch.tensor(payoff_at_stop).float(), 1)
+        dataset = TensorDataset(model_input, targets)
         dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=4)
 
         # defining the models; copying weights from previous model
@@ -55,24 +46,25 @@ def train(
         trainer = pl.Trainer(max_epochs=MAX_EPOCHS, enable_model_summary=False)
         trainer.fit(model, dataloader)
 
-        # saving the model
-        models[f"model_{n}"] = model
-
-        # updating optimal stopping time if stopping is larger than the approximated continuation value
-        idx = (
-            payoff_fn(n, x_n)
-            >= model(torch.tensor(np.c_[x_n, payoff_fn(n, x_n)]).float())
+        continuation_values = (
+            model(torch.tensor(np.c_[x_n, payoff_fn(n, x_n)]).float())
             .detach()
             .numpy()
             .squeeze()
         )
-        stopping_times[idx] = n
 
-        # del dataset
-        # del dataloader
-        # gc.collect()
+        # updating optimal stopping time if stopping is larger than the approximated continuation value
+        idx = payoff_now >= continuation_values
+        payoff_at_stop[idx] = payoff_now[idx]
+
+        # saving the model
+        models[f"model_{n}"] = model
+
+        del dataset
+        del dataloader
+        gc.collect()
 
     # value at time 0 (price) given by mean payoff at optimal stopping times
-    models["model_0"] = payoff_fn(stopping_times, stopped_paths).mean()
+    models["model_0"] = payoff_at_stop.mean()
 
     return models
